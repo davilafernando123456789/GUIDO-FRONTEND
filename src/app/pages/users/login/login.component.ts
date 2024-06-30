92adc16f776f847f71be2eb0b78837f7d0de5830
Cristina <ana.mendoza@tecsup.edu.pe>
2024-06-29 21:35:44 -0500

    Subiendo cambios en el login

--- src/app/pages/users/login/login.component.ts
+++ src/app/pages/users/login/login.component.ts
@@ -5,13 +5,14 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css', './adminlte.min.css' ],
+  styleUrls: ['./login.component.css', './adminlte.min.css'],
 })
 export class LoginComponent {
   usuario: string = '';
   password: string = '';
   mensaje: string = '';
   passwordFieldType: string = 'password';
+  submitted: boolean = false;
 
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -20,6 +21,11 @@ export class LoginComponent {
   }
 
   submitForm() {
+    this.submitted = true;
+    if (!this.usuario || !this.password) {
+      return;
+    }
+
     const data = { usuario: this.usuario, password: this.password };
     this.http
       .post<any>('http://localhost:4000/api/usuarios/auth', data)
@@ -66,103 +72,3 @@ export class LoginComponent {
       );
   }
 }
-
-// import { Component } from '@angular/core';
-// import { Router, NavigationExtras } from '@angular/router';
-// import { HttpClient } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css', './adminlte.min.css' ],
-// })
-// export class LoginComponent {
-//   usuario: string = '';
-//   password: string = '';
-//   mensaje: string = '';
-
-//   constructor(private http: HttpClient, private router: Router) {}
-
-//   submitForm() {
-//     const data = { usuario: this.usuario, password: this.password };
-//     this.http
-//       .post<any>('http://localhost:4000/api/usuarios/auth', data)
-//       .subscribe(
-//         (response) => {
-//           console.log('Respuesta del servidor:', response);
-//           if (response.mensaje === 'OK') {
-//             // Almacena los datos de sesión en sessionStorage
-//             sessionStorage.setItem('token', response.token);
-//             sessionStorage.setItem(
-//               'usuario',
-//               JSON.stringify({ ...response.usuario, rol: response.rol })
-//             );
-//             let navigationExtras: NavigationExtras = {
-//               state: { usuario: response.usuario },
-//             };
-
-//             if (response.rol === 1) {
-//               console.log('Redirigiendo al componente del alumno');
-//               sessionStorage.setItem('firstTimeLogin', 'false');
-//               this.router.navigate(['/home'], navigationExtras);
-//             } else if (response.rol === 2) {
-//               console.log('Redirigiendo al componente del profesor');
-//               sessionStorage.setItem('firstTimeLogin', 'false');
-//               this.router.navigate(['/home'], navigationExtras);
-//             } else if (response.rol === 3) {
-//               console.log('Redirigiendo al componente del admin');
-//               sessionStorage.setItem('firstTimeLogin', 'false');
-//               this.router.navigate(['/dashboard'], navigationExtras);
-//             } else {
-//               console.log('Credenciales incorrectas');
-//               this.mensaje = 'Credenciales incorrectas';
-//             }
-//           } else {
-//             console.log('Credenciales incorrectas');
-//             this.mensaje = 'Credenciales incorrectas';
-//           }
-//         },
-//         (error) => {
-//           console.error('Error al enviar los datos:', error);
-//           this.mensaje =
-//             'Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
-//         }
-//       );
-//   }
-// }
-
-
-  // submitForm() {
-  //   const data = {
-  //     usuario: this.usuario,
-  //     password: this.password,
-  //   };
-
-  //   this.http
-  //     .post<any>('http://localhost:4000/api/usuarios/auth', data)
-  //     .subscribe(
-  //       (response) => {
-  //         if (response.mensaje === 'OK') {
-  //           // Redirigir al componente correspondiente
-  //           if (response.rol === 1) {
-  //             // Redirigir al componente del alumno y pasar la información del usuario
-  //             this.router.navigate(['/coursesStudent'], {
-  //               state: { usuario: response.usuario },
-  //             });
-  //           } else if (response.rol === 2) {
-  //             // Redirigir al componente del profesor y pasar la información del usuario
-  //             this.router.navigate(['/coursesStudent'], {
-  //               state: { usuario: response.usuario },
-  //             });
-  //           }
-  //         } else {
-  //           this.mensaje = 'Credenciales incorrectas';
-  //         }
-  //       },
-  //       (error) => {
-  //         console.error('Error al enviar los datos:', error);
-  //         this.mensaje =
-  //           'Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
-  //       }
-  //     );
-  // }
